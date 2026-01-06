c2a04a2f2a89dafa9eba4b09fc98cc42cd151761
iiPython <ben@iipython.dev>
2026-01-06 01:38:54 -0600

    stuff

---
+++ gallery_cli.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+# Copyright (c) 2026 iiPython
+
+# Modules
+import json
+import subprocess
+from pathlib import Path
+from datetime import datetime, timezone
+
+import inquirer
+from inquirer.errors import ValidationError
+
+# Initialization
+KNOWN_UUIDS = {
+    "RomeoWasAsleep": "271f1987-57dc-4550-b7ad-a1b1c4bb1a7c",
+	"tonyz1111":      "2fb80ffb-b4e8-46be-86f3-e7eae1a6c38f",
+	"rockttlol":      "6102acab-975f-42bd-a720-144ca3995585",
+	"eunyung":        "617d0b8a-148f-47c0-b4f4-36dfe1ac79c7",
+	"iiPythonx":      "8018c39c-3d81-434e-a02d-6e0813340fd6",
+	"LittleHebo":     "a695d266-e3fe-4931-a961-0d8eafc0385d",
+	"ernistus":       "bf0f7598-e7fe-4e09-b7a2-a1289d907bbd",
+	"DmmDGaming":     "d9736929-859d-478a-8575-02cc397673d7"
+}
+KNOWN_USERS = sorted(KNOWN_UUIDS.keys())
+
+# Helping methods
+def process_timestamp(timestamp: str) -> str:
+    try:
+        date = datetime.fromisoformat(timestamp)
+
+    except ValueError:
+        date = datetime.strptime(timestamp, "%m/%d/%y %I:%M %p").replace(tzinfo = timezone.utc)
+
+    return date.isoformat(timespec = "milliseconds")[:-6] + "Z"
+
+def validate_timestamp(_, timestamp: str) -> bool:
+    try:
+        process_timestamp(timestamp)
+        return True
+
+    except ValueError:
+        raise ValidationError("", reason = "Invalid timestamp given.")
+
+# Handle auto detection
+for file in Path("data").rglob("*.avif"):
+    data_file = file.with_suffix(".json")
+    if data_file.is_file():
+        continue
+
+    season_id = file.parents[1].name
+    season_data = json.loads((file.parents[1] / "season.json").read_text())
+
+    # Launch image in current window
+    subprocess.run(["vscodium", file.absolute()])
+
+    # Ask our questions
+    answers = inquirer.prompt([
+        inquirer.Text("name", message = "Screenshot Name", validate = lambda _, v: v.strip()),
+        inquirer.Text("description", message = "Short Description", validate = lambda _, v: v.strip()),
+        inquirer.Text("time", message = "Timestamp", validate = validate_timestamp, default = season_data["since"]),
+        inquirer.List(
+            "camera",
+            message = "Who took it?",
+            choices = KNOWN_USERS
+        )
+    ])
+    if answers is None:
+        break
+
+    # Save data to disk
+    data_file.write_text(json.dumps({
+        "camera": KNOWN_UUIDS[answers["camera"]],
+        "description": answers["description"],
+        "filename": file.name,
+        "name": answers["name"],
+        "season": season_id,
+        "time": process_timestamp(answers["time"]),
+        "url": f"/data/{season_id}/gallery/{file.name}"
+    }, indent = 4))
