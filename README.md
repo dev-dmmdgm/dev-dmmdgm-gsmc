9a8be3e947eb23b720bc5b4102e985edc9eef4ce
DmmDGM <dm12332131mdgaming@gmail.com>
2026-01-05 19:29:48 -0500

    fixed client and readme

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
-# proposal
-
-/data
-/data/s1
-/data/s1/gallery
-
-/api/archives
-/api/archives/:season
-/api/galleries
-/api/galleries/:season
-/api/files/
-/api/gsmc-pack
+# yuhh
---
+++ src/client.tsx
@@ -0,0 +1,12 @@
+// Imports
+import { render } from "preact";
+
+// Creates app
+function App() {
+    return <div>
+        haii
+    </div>;
+}
+
+// Renders app
+render(<App/>, document.getElementById("app")!);
