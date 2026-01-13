0dd1dbd272f69df8ef9ae80d614e5699cb24d94e
DmmDGM <dm12332131mdgaming@gmail.com>
2026-01-13 00:12:11 -0500

    damn

--- src/profiles.ts
+++ src/profiles.ts
@@ -2,10 +2,6 @@
 import nodePath from "node:path";
 import { Archive, Profile } from "./type";
 
-// Creates file
-const file = Bun.file("src/profiles.json");
-const profilesJSON = await file.exists() ? await file.json() : [];
-
 // Fetches uuids
 const data = nodePath.resolve("data");
 const glob = new Bun.Glob(nodePath.resolve(data, "*", "season.json"));
@@ -14,16 +10,23 @@ const archives = await Array.fromAsync(filenames.map((filename) => Bun.file(file
 
 // Fetches profiles
 const uuids = Array.from(new Set(archives.flatMap((archive) => archive.players)));
-const players = await Array.fromAsync(uuids.map(async (uuid) => { 
-    const query = await fetch(new URL(`/minecraft/profile/lookup/${uuid}`, "https://api.minecraftservices.com"));
-    if(!query.ok) return null;
-    return await query.json() as { id: string; name: string; };
+const players = await Array.fromAsync(uuids.map(async (uuid) => {
+    for(let i = 0; i < 5; i++) {
+        const query = await fetch(new URL(`/minecraft/profile/lookup/${uuid}`, "https://api.minecraftservices.com"));
+        if(query.status === 429) {
+            console.log("Ratelimit reached, waiting 5 seconds...");
+            await Bun.sleep(5000);
+            continue;
+        }
+        return query.ok ? await query.json() as { id: string; name: string; } : null;
+    }
+    return null;
 }));
-const profiles: Profile[] = Object.assign([], profilesJSON, players.filter((player) => player !== null).map((player) => ({
+const profiles: Profile[] = players.filter((player) => player !== null).map((player) => ({
     avatarURL: new URL(`/avatar/${player.id}`, "https://mc-heads.net").toString(),
     username: player.name,
     uuid: player.id
-})));
+}));
 
 // Writes profiles
 await Bun.file("src/profiles.json").write(JSON.stringify(profiles, null, 4));
